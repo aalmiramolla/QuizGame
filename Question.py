2148990c46ec66d2e79f37f62141edba5894b8c9
Alejandro <aalmiramolla@gmail.com>
2015-08-10 16:21:22 +0200

    Empezando a diseñar una app para examenes

--- Question.py
+++ Question.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 class Question:
-	def __init__(self,anC,question,an1,an2,an3,an4):
-		self.question=question
-		self.answer1=an1
-		self.answer2=an2
-		self.answer3=an3
-		self.answer4=an4
-		self.correctanswer=anC
-		self.correct=False
-		self.playeranswer="E"
+        def __init__(self, question, answers, correctAnswer, topic, image=None):
+                self.question = question
+                self.correctanswer = correctAnswer
+                self.answers = answers
+                self.topic = topic
+                self.image = image
 		
-	def checkAnswer(self):
-		if (self.playeranswer == self.correctanswer):
-			self.correct=True
-		
-	def setPlayerAnswer(self):
-		self.playeranswer=str(raw_input("Youre answer-->"))
-		self.checkAnswer()
-
-	def printQuestion(self):
-		print self.question
-		print "\tA) "+self.answer1
-		print "\tB) "+self.answer2
-		print "\tC) "+self.answer3
-		print "\tD) "+self.answer4
-		while (self.playeranswer<"A" or self.playeranswer>"D"):
-			self.setPlayerAnswer()
-			
+        def checkAnswer(self, answer):
+                if (answer == self.correctAnswer):
+                        self.correct=True			
